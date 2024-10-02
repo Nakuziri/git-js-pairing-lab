54aa6d0df6d51731e3906592cc229d82434d08bc
Xavier Campos <xcampos@brooklynsteamcenter.org>
2024-10-02 13:58:58 -0400

    Finished first function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //Code your solutions in this file
+
+//Function 1 
+const fiveToOneHundred = () => {
+    for( let i = 5; i < 101; i += 1){
+       console.log(i); 
+    }
+};
+
+fiveToOneHundred();
