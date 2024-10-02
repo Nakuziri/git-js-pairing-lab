f87a418fd2b591a25ede3b36cab3b5ac710b4e9f
sway-codes <primaryjosue28@gmail.com>
2024-10-02 14:32:19 -0400

    function 2 is solved

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const fiveToOneHundred = () => {
 };
 
 fiveToOneHundred();
+
+// Function 2 
+const multiplesOfThree = () => {
+ for( let i =1; i < 101; i += 1) {
+    if( i % 3 === 0){
+        console.log(i);
+    }
+ } 
+};
+
+multiplesOfThree();
